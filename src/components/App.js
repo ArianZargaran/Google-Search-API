cbbe092419a51055d035d3a88e5081568782424a
Arian Zargaran <arianzf@gmail.com>
2018-01-15 22:28:51 -0800

    Creating Redux set up

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ export default class App extends Component {
         title: "This is a title",
         url: `www.LoremIpsum.com`,
         description: "Coordinates: 36°17′56.4″N 30°21′17.3″E﻿ / ﻿36.299000°N 30.354806°E﻿ / 36.299000; 30.354806 Gagae (Greek: Γάγαι), was a town on the southeast coast of Lycia, in what is now the province of Antalya, from which the Gagates lapis derived its name. The ruins are located in Kumluca district, Antalya Province, Turkey.",
-        emotion: `😒 10 | 😃 50% | 😱 35% | 🙈 5% | 😡 0%`
+        emotion: `😒 10% | 😃 50% | 😱 35% | 🙈 5% | 😡 0%`
       },
       {
         title: "This is a title",
